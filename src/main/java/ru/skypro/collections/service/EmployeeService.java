e359ad23b5aa32ebf54c7b333866d5869884a45c
WalterMitty23 <23alexold23@gmail.com>
2023-11-05 13:31:01 -0500

    The work is done

--- src/main/java/ru/skypro/collections/service/EmployeeService.java
+++ src/main/java/ru/skypro/collections/service/EmployeeService.java
@@ -2,9 +2,15 @@ package ru.skypro.collections.service;
 
 import ru.skypro.collections.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
 
     Employee add(String firstName, String lastName);
-    void remove(String firstName, String lastName);
+    Employee remove(String firstName, String lastName);
     Employee find(String firstName, String lastName);
+
+    List<Employee> getAll();
+
+
 }
